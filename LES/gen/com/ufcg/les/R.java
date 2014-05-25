7278fab15c95c9dc898a3dbfc19b72e5a4a5afcf
Werton Guimarães <werton007@gmail.com>
2014-05-25 00:05:55 -0300

    Organização de pacotes, Criação de Algumas Classes e conexão com o Servidor

--- LES/gen/com/ufcg/les/R.java
+++ LES/gen/com/ufcg/les/R.java
@@ -61,17 +61,13 @@ public final class R {
         public static final int text_field=0x7f02001d;
     }
     public static final class id {
-        public static final int BotaoCancelar=0x7f090005;
-        public static final int Button_Sum=0x7f090000;
-        public static final int CampoNumero1=0x7f090003;
-        public static final int CampoNumero2=0x7f090002;
-        public static final int action_settings=0x7f090006;
-        public static final int botaoOK=0x7f090004;
-        public static final int result_sum=0x7f090001;
+        public static final int action_settings=0x7f090003;
+        public static final int capturarJSON=0x7f090001;
+        public static final int result_JSON=0x7f090002;
+        public static final int salvarJSON=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int sum=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f080000;
