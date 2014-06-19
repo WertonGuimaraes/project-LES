94935e73008241b8ca45e076448d0b533453cdcf
carolinass <carolzinhacabral@gmail.com>
2014-06-18 23:48:44 -0300

    Activity da verificacao do registro de atividade no dia anterior

---
+++ LES/src/com/ufcg/les/VerificaTarefasActivity.java
@@ -0,0 +1,42 @@
+package com.ufcg.les;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class VerificaTarefasActivity extends Activity {
+
+	//private ImageView imageView1;
+	//private TextView textView1;
+	private Button adicionarTI;
+	private Button paginaInicial;
+	
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.verifica_criacao_tarefas);
+		
+		adicionarTI = (Button)findViewById(R.id.Button_AddTI2);
+		adicionarTI.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(VerificaTarefasActivity.this,AdicionarTIActivity.class);
+				startActivity(i);
+				finish();
+			}
+		});
+		
+		paginaInicial = (Button)findViewById(R.id.Voltar);
+		paginaInicial.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(VerificaTarefasActivity.this,MainActivity.class);
+				startActivity(i);
+				finish();
+			}
+		});
+	}
+}
